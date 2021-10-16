a6981fc4a492a61bec45adfc2a5fe7e4efbfb4dd
Matheus Girotto <girotto.matheus@gmail.com>
2021-10-16 16:49:15 -0300

    add func to create initial centroids

--- aux_functions.py
+++ aux_functions.py
@@ -1,36 +1,58 @@
 import numpy as np
 
-def create_sinthetic_data(n_dim: int, N_vector: int, n_cluster: int, max_range: int) -> list:
+def create_synthetic_data(n_dim: int, N_vectors: int, k_clusters: int, max_range: int) -> list:
     """
-    Make sinthetic data to apply k-means algol.
+    Make synthetic data to apply k-means algol.
     
-    IN:
-    n_dim     --> the dimension of the sinthetic vectors
-    N_vector  --> the number of vectors to make
-    n_cluster --> the number of artificial clusters from which will be created the vecs
-    max_range --> the range of the centroid coordinates
+    INput:
+    n_dim      --> the dimension of the synthetic vectors
+    N_vectors  --> the number of vectors to make
+    k_clusters --> the number of artificial clusters from which will be created the vecs
+    max_range  --> the range of the centroid coordinates
 
-    OUT:
+    OUTput:
     data --> list of vectors to which we will apply k-means
     """
     # list of arrays containing the artificial centroids
     centroids = [] 
 
     # sample the random centroids from uniform distribution from -max_range to max_range
-    # loop over clusters to create n_cluster multi-dim centroids
-    for i in range(n_cluster):
+    # loop over clusters to create k_clusters multi-dim centroids
+    for i in range(k_clusters):
         centroids.append(max_range * (2 * np.random.random((n_dim,)) - 1))
 
     # list cointaning the data points (vectors)
     data = []
-    for i in range(N_vector):
+    for i in range(N_vectors):
         # sample to what cluster the random vector will be from
-        cluster_number = np.random.randint(1, n_cluster + 1)
+        cluster_number = np.random.randint(1, k_clusters + 1)
         # create the data from normal distribution
         vector = np.random.randn(n_dim) + centroids[cluster_number - 1]
         
         data.append(vector)
 
     return data
+
+def create_init_centroids(data: list, k_clusters: int) -> list:
+    """This function returns initial random centroids in the appropiate range for better 
+    convergence of the algorithm.
     
+    INput:
+    data      --> list of vectors that will be clustered
+    k_cluster --> the number of clusters
+
+    OUTput:
+    list --> the initial centroids to the algol iterate over.  
+    """
+    # stack vectors to get the maxima in each of their dimensions
+    X = np.vstack(data[:])
+    maxima = np.max(np.abs(X), axis=0)
+    
+    # extract the dim of the vectors to create centroids with same dim
+    n_dim = len(data[0])
+    
+    # centroids sampled at random from continuos PDF with range maxima of each vecs' dimensions
+    centroids = [maxima * (2 * np.random.random(n_dim) - 1) for _ in range(k_clusters)]
+
+    return centroids
     
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+# scientific packages
 import numpy as np
 import matplotlib.pylab as plt
+
+# auxiliary functions
 from aux_functions import *
 
-data = create_sinthetic_data(n_dim=2, N_vector=1024, n_cluster=4, max_range=16)
+# hyperparameters and input of algorithm
+from parameters_INput import *
+
+# creates synthetic data
+data = create_synthetic_data(n_dim, N_vectors, k_clusters, max_range)
+# initializes random centroids
+centroids = create_init_centroids(data, k_clusters)
 
 # X = np.vstack(data[:])
 # print(X.shape)
